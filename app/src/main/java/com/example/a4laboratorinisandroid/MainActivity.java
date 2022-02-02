831b879c09ea9e6e4cd7a47b9f819e24cd859d99
Karolis161 <karolisurbaitis59910@gmail.com>
2022-02-02 23:19:40 +0200

    Code

---
+++ app/src/main/java/com/example/a4laboratorinisandroid/MainActivity.java
@@ -0,0 +1,43 @@
+package com.example.a4laboratorinisandroid;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+import android.widget.Button;
+import android.widget.EditText;
+
+public class MainActivity extends AppCompatActivity {
+    private WebView webView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        webView = (WebView) findViewById(R.id.webView);
+
+        Button button = (Button) findViewById(R.id.button);
+        EditText editText = (EditText) findViewById(R.id.editText);
+
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String link = editText.getText().toString();
+                webView.setWebViewClient(new WebViewClient());
+                webView.loadUrl(link);
+            }
+        });
+    }
+
+    @Override
+    public void onBackPressed() {
+        if (webView.canGoBack()) {
+            webView.goBack();
+        } else {
+            super.onBackPressed();
+        }
+    }
+}
